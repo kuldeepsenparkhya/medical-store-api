463e059c6a36e94e6b68b7426ee2e633baa58d5c
Parkhya.itpark <parkhya.itparkh@parkhya.com>
2024-09-11 14:12:54 +0530

    Update code here

--- app/routes/coin.js
+++ app/routes/coin.js
@@ -3,8 +3,8 @@ const { coins } = require('../controllers');
 const { adminAccess, authJWT } = require('../middlewares/auth');
 
 module.exports = app => {
-    router.post('/loyality', authJWT, adminAccess, coins.updateCoinConversion)
-    router.get('/loyality', coins.getCoin)
+    router.patch('/loyalities', authJWT, adminAccess, coins.updateCoinConversion)
+    router.get('/loyalities', coins.getCoin)
 
     app.use('/api', router);
 }
